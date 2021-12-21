9a2717ef9a0c9999742f6bd00c57cd66af58818e
GCC20000 <93943048+GCC20000@users.noreply.github.com>
2021-12-21 16:43:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var engine 
+var world
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Crie os Corpos Aqui.
+	var ground_options={
+		isStatic:true
+	}
+	ground=Bodies.rectangle(400,650,800,40,ground_options)
+	World.add(world,ground);
+	
+	var block1_options={
+		restitution:0.5,
+		friction:0.02,
+		frictionAir:0
+	}
+	block1 = Bodies.circle(220,10,10,block1_options)
+	World.add(world,block1);
+
+	var block2_options={
+		restitution:0.7,
+		friction:0.01,
+		frictionAir:0.1
+	}
+	block2 = Bodies.rectangle(110,50,10,10,block2_options)
+	World.add(world,block2);
+
+	var block3_options = {
+		restitution:0.01,
+		friction:1,
+		frictionAir:0.3
+	}
+	block3 = Bodies.rectangle(350,50,10,10,block3_options);
+	World.add(world,block3);
+
+	rectMode(CENTER)
+	ellipseMode(RADIUS)
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  rect(ground.position.x,ground.position.y,800,20)
+  rect(block2.position.x,block2.position.y,50,50)
+  rect(block3.position.x,block3.position.y,50,50)
+  ellipse(block1.position.x,block1.position.y,20)
+}
+
+
+
